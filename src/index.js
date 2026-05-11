1bd659d711a9eaa8137da7e0f2aadeaf09ecb4f9
Nathaniel Ankomah Aidoo <nathanielankomahaidoo@gmail.com>
2026-05-11 12:57:58 +0000

    add css and image webpack laders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import odinImage from "./odin-lined.png"
+import "./styles.css";
 import {greeting} from "./greeting.js";
+
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
 
 console.log(greeting);
